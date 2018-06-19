0ea2dc6ef34a87faca5c0d0fe000369372631eb2
Carson Kelley <carsonmkelley@gmail.com>
2018-06-19 14:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function createRock(x) {
     /**
      * If a rock collides with the DODGER,
      * we should call endGame()
-     */
+     *
     if(checkCollision(rock)) {
       endGame();
     } else if (rock.style.top < 380) {
